57955fc06e848328b0ac8f6de0cbb2d6509b2988
Nart <daniel.abzakh@gmail.com>
2022-11-02 15:07:02 +0300

    Added length

--- yargi/tsv2json.py
+++ yargi/tsv2json.py
@@ -5,7 +5,9 @@ df['start_sec'] = round(pd.to_timedelta(df['start']).dt.total_seconds(),3)
 df['end_sec'] = round(pd.to_timedelta(df['end']).dt.total_seconds(),3)
 df['duration'] = round(df['end_sec'] - df['start_sec'],3)
 df['clip'] = [ str(i+1) for i in range(0, (len(df.index)), 1) ]
-df.to_csv('caption-edited.tsv', sep='\t', quoting=3, index=False, columns = ['clip','start_sec','duration','sentence','gender'])
+df['length'] = [ len(df['sentence'][i]) for i in range(0, (len(df.index)), 1) ]
+
+df.to_csv('caption-edited.tsv', sep='\t', quoting=3, index=False, columns = ['clip','start_sec','duration','sentence','gender','length'])
 
 import json
 
