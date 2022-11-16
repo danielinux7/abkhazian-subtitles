ea33073bdce898cca7272c79457873024ae52cb0
Nart <daniel.abzakh@gmail.com>
2022-11-16 13:35:40 +0300

    Added noise profile

--- yargi/sbv2tsv.sh
+++ yargi/sbv2tsv.sh
@@ -1,5 +1,8 @@
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^(.*),.*$/\1/g' > starttime.txt
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^.*,(.*)$/\1/g' > endtime.txt
 grep -v '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -z -r 's/\n\n/empty/g' | sed -z -r 's/\n/ /g' | sed -z -r 's/empty/\n/g' > text.txt
-paste starttime.txt endtime.txt text.txt > caption.tsv
-rm starttime.txt endtime.txt text.txt
+echo -e "0:00:00.000\t0:00:20.000\t...АУАДА АБЖЬЫ..." > noise.tsv 
+echo -e "0:00:00.000\t0:00:20.000\t...АУАДА АБЖЬЫ..." >> noise.tsv 
+paste starttime.txt endtime.txt text.txt > caption2.tsv
+cat noise.tsv caption2.tsv > caption.tsv
+rm starttime.txt endtime.txt text.txt caption2.tsv noise.tsv
--- yargi/tsv2json.py
+++ yargi/tsv2json.py
@@ -6,7 +6,8 @@ df['end_sec'] = round(pd.to_timedelta(df['end']).dt.total_seconds(),3)
 df['duration'] = round(df['end_sec'] - df['start_sec'],3)
 df['clip'] = [ str(i+1) for i in range(0, (len(df.index)), 1) ]
 df['length'] = [ len(df['sentence'][i]) for i in range(0, (len(df.index)), 1) ]
-
+df.iloc[0, 3] = 'm'
+df.iloc[1, 3] = 'f'
 df.to_csv('caption-edited.tsv', sep='\t', quoting=3, index=False, columns = ['clip','start_sec','duration','sentence','gender','length'])
 
 import json
