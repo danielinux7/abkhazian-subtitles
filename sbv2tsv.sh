b74fc7abc67079e53d530a4e37cb9a91a324e4b5
Nart <daniel.abzakh@gmail.com>
2022-12-13 15:10:34 +0300

    added mc subs

--- sbv2tsv.sh
+++ sbv2tsv.sh
@@ -1,8 +1,5 @@
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^(.*),.*$/\1/g' > starttime.txt
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^.*,(.*)$/\1/g' > endtime.txt
 grep -v '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -z -r 's/\n\n/empty/g' | sed -z -r 's/\n/ /g' | sed -z -r 's/empty/\n/g' > text.txt
-echo -e "0:00:00.000\t0:00:20.000\t...АУАДА АБЖЬЫ 1..." > noise.tsv 
-echo -e "0:00:00.000\t0:00:20.000\t...АУАДА АБЖЬЫ 2..." >> noise.tsv 
-paste starttime.txt endtime.txt text.txt > caption2.tsv
-cat noise.tsv caption2.tsv > caption.tsv
-rm starttime.txt endtime.txt text.txt caption2.tsv noise.tsv
+paste starttime.txt endtime.txt text.txt > caption.tsv
+rm starttime.txt endtime.txt text.txt
