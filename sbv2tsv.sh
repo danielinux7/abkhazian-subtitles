22b3a60a2c25dfd7fd062abc9a27cd7c598434fb
Nart <daniel.abzakh@gmail.com>
2023-01-31 09:41:27 +0300

    restructure

--- sbv2tsv.sh
+++ sbv2tsv.sh
@@ -1,5 +1,6 @@
-#sed -r '/^[0-9]+$/d' caption.srt > caption.sbv
+#sed -r '/^[0-9]+[\r\n]/d' caption.srt > caption.sbv
 #sed -i -r 's/([0-9]+:[0-9]+:[0-9]+),([0-9]+)/\1.\2/g' caption.sbv
+#sed -i -r 's/([0-9]+:[0-9]+:[0-9]+\.[0-9]+) --> ([0-9]+:[0-9]+:[0-9]+\.[0-9]+)/\1,\2/g' caption.sbv
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^(.*),.*$/\1/g' > starttime.txt
 grep '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -r 's/^.*,(.*)$/\1/g' > endtime.txt
 grep -v '^[0-9]:[0-9][0-9]:.*[0-9]$' caption.sbv | sed -z -r 's/\n\n/empty/g' | sed -z -r 's/\n/ /g' | sed -z -r 's/empty/\n/g' > text.txt
